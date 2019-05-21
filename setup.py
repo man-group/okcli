605b8f2e0490fee1416b702b784a51c27f4703e8
Chris Boddy <Christopher.Boddy@man.com>
2019-05-21 16:49:40 +0100

    inline requirements and bump version

--- setup.py
+++ setup.py
@@ -6,22 +6,14 @@ import re
 
 from setuptools import find_packages, setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('okcli/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+import okcli
+version = okcli.__version__
 
 description = 'A CLI for Oracle DB Database with auto-completion and syntax highlighting.'
 
 def get_long_description():
     with open('README.md', 'r') as f:
         return f.read()
-
-
-def get_requirements():
-    with open('requirements.txt', 'r') as f:
-        return f.read().splitlines()
 
 
 setup(
@@ -31,9 +23,20 @@ setup(
     author_email='ManAHLTech@ahl.com',
     url='https://github.com/manahl/okcli',
     packages=find_packages(),
+    description=description,
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
-    install_requires=get_requirements(),
+    install_requires=[
+        'cx_Oracle',
+        'cli_helpers >= 0.1.0,<=0.2.3',
+        'click >= 4.1',
+        'Pygments >= 1.6',
+        'prompt_toolkit==1.0.14',
+        'sqlparse>=0.2.2,<0.3.0',
+        'configobj >= 5.0.5',
+        'pytest',
+        'mock',
+        ],
     include_package_data=True,
     entry_points={
         'console_scripts': ['okcli = okcli.main:cli'],
@@ -53,6 +56,6 @@ setup(
         'Topic :: Database :: Front-Ends',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
-],
+    ],
 )
 
